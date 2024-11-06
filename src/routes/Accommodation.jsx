1bd225d7d648c711f40b7da92f80d884780b861f
noscatis <michael.bocquel@gmail.com>
2024-11-06 19:59:57 +0100

    style: add basic styling to pages

---
+++ src/routes/Accommodation.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Accomodation() {
+	return (
+		<>
+			<Header />
+			<p>Accommodation</p>
+			<Footer />
+		</>
+	);
+}
---
+++ src/routes/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function ErrorPage() {
+	return (
+		<>
+			<Header />
+			<p>Error</p>
+			<Footer />
+		</>
+	);
+}
