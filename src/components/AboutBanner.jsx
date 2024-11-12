8ea942938926996cc60e723f9f1f47f65c8e6b5e
noscatis <michael.bocquel@gmail.com>
2024-11-12 14:22:30 +0100

    feat(component): add banner in home page and about page

---
+++ src/components/AboutBanner.jsx
@@ -0,0 +1,7 @@
+export default function AboutBanner() {
+	return (
+		<div className="about__banner__container">
+			<img src="../src/assets/about-banner.jpg" className="about__banner__image" />
+		</div>
+	);
+}
---
+++ src/components/HomeBanner.jsx
@@ -0,0 +1,8 @@
+export default function HomeBanner() {
+	return (
+		<div className="home__banner__container">
+			<img src="../src/assets/home-banner.jpg" className="home__banner__image" />
+			<p className="home__banner__text">Chez vous, partout et ailleurs</p>
+		</div>
+	);
+}
