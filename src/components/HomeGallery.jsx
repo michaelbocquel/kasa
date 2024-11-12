cf8c6c31ffffce9f81e495c4b75e8b07b7bfd96c
noscatis <michael.bocquel@gmail.com>
2024-11-12 14:27:15 +0100

    feat: update home gallery component

---
+++ src/components/HomeGallery.jsx
@@ -0,0 +1,14 @@
+import data from "../data/data.json";
+
+export default function HomeGallery() {
+	return (
+		<div className="home__gallery__container">
+			{data.map((item) => (
+				<div className="home__gallery__image__container" key={item.id}>
+					<img src={item.cover} className="home__gallery__image"></img>
+					<p className="home__gallery__title">{item.title}</p>
+				</div>
+			))}
+		</div>
+	);
+}
