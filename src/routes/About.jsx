eec8df02fa43157677a0c1810f5dbb599e9adb8a
noscatis <michael.bocquel@gmail.com>
2024-11-12 14:26:37 +0100

    feat: update about page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,11 +1,16 @@
 import Header from "../components/Header";
+import AboutBanner from "../components/AboutBanner";
+import AboutValues from "../components/AboutValues";
 import Footer from "../components/Footer";
 
 export default function About() {
 	return (
 		<>
 			<Header />
-			<p>About</p>
+			<div className="about__main__container">
+				<AboutBanner />
+				<AboutValues />
+			</div>
 			<Footer />
 		</>
 	);
