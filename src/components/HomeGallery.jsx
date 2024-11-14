b966183086e474a97187dcdedb39b6b0dfdd6237
noscatis <michael.bocquel@gmail.com>
2024-11-14 15:19:29 +0100

    feat: add router to individual accommodations pages

--- src/components/HomeGallery.jsx
+++ src/components/HomeGallery.jsx
@@ -3,11 +3,15 @@ import data from "../data/data.json";
 export default function HomeGallery() {
 	return (
 		<div className="home__gallery__container">
-			{data.map((item) => (
-				<div className="home__gallery__image__container" key={item.id}>
+			{data.map((item, key) => (
+				<a
+					href={`/accommodation/${key + 1}`}
+					className="home__gallery__image__container"
+					key={item.id}
+				>
 					<img src={item.cover} className="home__gallery__image"></img>
 					<p className="home__gallery__title">{item.title}</p>
-				</div>
+				</a>
 			))}
 		</div>
 	);
