b820b059a018c45924af4ca8bb41d3cd73b7c7cf
noscatis <michael.bocquel@gmail.com>
2024-11-14 15:20:25 +0100

    feat: add data elements to individual accommodations pages

--- src/routes/Accommodation.jsx
+++ src/routes/Accommodation.jsx
@@ -1,11 +1,23 @@
 import Header from "../components/Header";
+import data from "../data/data.json";
 import Footer from "../components/Footer";
+import { useParams } from "react-router-dom";
 
 export default function Accomodation() {
+	let { id } = useParams();
+
 	return (
 		<>
 			<Header />
-			<p>Accommodation</p>
+			<img src={data[id - 1].cover} className="home__gallery__image"></img>
+			<h1>{data[id - 1].title}</h1>
+			<h1>{data[id - 1].location}</h1>
+			<h1>{data[id - 1].tags}</h1>
+			<h1>{data[id - 1].description}</h1>
+			<h1>{data[id - 1].equipments}</h1>
+			<h1>{data[id - 1].host.name}</h1>
+			<img src={data[id - 1].host.picture}></img>
+			<h1>{data[id - 1].rating}</h1>
 			<Footer />
 		</>
 	);
