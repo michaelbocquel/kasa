cfba96f616015ea7c534eb3dada863f4cbdcdc9d
noscatis <michael.bocquel@gmail.com>
2024-11-18 17:14:09 +0100

    feat: add state to accommodation page

--- src/routes/Accommodation.jsx
+++ src/routes/Accommodation.jsx
@@ -2,23 +2,33 @@ import Header from "../components/Header";
 import data from "../data/data.json";
 import Footer from "../components/Footer";
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 export default function Accomodation() {
-	let { id } = useParams();
-
+	const { id } = useParams();
+	const [accommodation] = useState(data.filter((logement) => logement.id === id));
 	return (
 		<>
-			<Header />
-			<img src={data[id - 1].cover} className="home__gallery__image"></img>
-			<h1>{data[id - 1].title}</h1>
-			<h1>{data[id - 1].location}</h1>
-			<h1>{data[id - 1].tags}</h1>
-			<h1>{data[id - 1].description}</h1>
-			<h1>{data[id - 1].equipments}</h1>
-			<h1>{data[id - 1].host.name}</h1>
-			<img src={data[id - 1].host.picture}></img>
-			<h1>{data[id - 1].rating}</h1>
-			<Footer />
+			{accommodation.length > 0 ? (
+				<>
+					<Header />
+					<img
+						src={accommodation[0].cover}
+						className="home__gallery__image"
+					></img>
+					<h1>{accommodation[0].title}</h1>
+					<h1>{accommodation[0].location}</h1>
+					<h1>{accommodation[0].tags}</h1>
+					<h1>{accommodation[0].description}</h1>
+					<h1>{accommodation[0].equipments}</h1>
+					<h1>{accommodation[0].host.name}</h1>
+					<img src={accommodation[0].host.picture}></img>
+					<h1>{accommodation[0].rating}</h1>
+					<Footer />
+				</>
+			) : (
+				<p>Erreur</p>
+			)}
 		</>
 	);
 }
