9f340c6adc1c760d69f8dd473d59aaa3d0d1fb8e
noscatis <michael.bocquel@gmail.com>
2024-11-20 14:28:12 +0100

    feat: add alt attribute to header logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,20 @@ import { NavLink } from "react-router-dom";
 
 export default function Header() {
 	return (
-		<>
-			<header className="header">
-				<img src="../src/assets/header-logo.svg" alt="" className="header__logo" />
-				<nav className="header__nav">
-					<NavLink to="/" className="header__nav__link">
-						Accueil
-					</NavLink>
-					<NavLink to="/about" className="header__nav__link">
-						A propos
-					</NavLink>
-				</nav>
-			</header>
-		</>
+		<header className="header">
+			<img
+				src="../src/assets/header-logo.svg"
+				alt="Red Kasa logo on white background"
+				className="header__logo"
+			/>
+			<nav className="header__nav">
+				<NavLink to="/" className="header__nav__link">
+					Accueil
+				</NavLink>
+				<NavLink to="/about" className="header__nav__link">
+					A propos
+				</NavLink>
+			</nav>
+		</header>
 	);
 }
