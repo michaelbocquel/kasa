e046709b2921ff0f2953ec6c9aad3826cea91ab6
noscatis <michael.bocquel@gmail.com>
2024-11-20 14:27:47 +0100

    feat: replace a tag to link tag for link to home page on error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Error() {
 	return (
 		<div className="error__display">
 			<p className="error__display__title">404</p>
-			<p className="error__display__text">Oups! La page que vous demandez n'existe pas.</p>
-			<a href="/" className="error__display__home__link">Retourner sur la page d'accueil</a>
+			<p className="error__display__text">
+				Oups! La page que vous demandez n'existe pas.
+			</p>
+			<Link to="/" className="error__display__home__link">
+				Retourner sur la page d'accueil
+			</Link>
 		</div>
 	);
 }
