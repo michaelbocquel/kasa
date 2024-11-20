312ed5832246acdd81e441a504a8ed209daf905c
noscatis <michael.bocquel@gmail.com>
2024-11-20 14:29:45 +0100

    feat: replace className to use props

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,8 +1,8 @@
 export default function Collapse(props) {
-  return (
-    <details className="collapse__details">
-      <summary className="collapse__summary">{props.summary}</summary>
+	return (
+		<details className={props.class}>
+			<summary className="collapse__summary">{props.summary}</summary>
 			{props.details}
-    </details>
-  );
+		</details>
+	);
 }
