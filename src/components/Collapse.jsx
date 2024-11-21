65ecdd97e0b02d0c46d8a10e4c5acadf45f26f18
noscatis <michael.bocquel@gmail.com>
2024-11-21 17:22:05 +0100

    feat: change class props of details and summary to style them independently

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,7 +1,7 @@
 export default function Collapse(props) {
 	return (
-		<details className={props.class}>
-			<summary className="collapse__summary">{props.summary}</summary>
+		<details className={props.classDetails}>
+			<summary className={props.classSummary}>{props.summary}</summary>
 			{props.details}
 		</details>
 	);
