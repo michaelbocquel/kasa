f0fd10a0cc40874ff3cb96bd26a6570c00487d03
noscatis <michael.bocquel@gmail.com>
2024-11-21 15:14:20 +0100

    refactor: rename AboutValues to Values

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,23 +1,29 @@
 import Collapse from "./Collapse";
 
-export default function AboutValues() {
+export default function Values() {
 	return (
-		<div className="about__values__container">
+		<div className="values__container">
 			<Collapse
 				summary="Fiabilité"
 				details="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+				classSummary="collapse__summary"
+				classDetails="collapse__details"
 			/>
 			<Collapse
 				summary="Respect"
 				details="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
 					comportement discriminatoire ou de perturbation du voisinage
 					entraînera une exclusion de notre plateforme."
+				classSummary="collapse__summary"
+				classDetails="collapse__details"
 			/>
 			<Collapse
 				summary="Service"
 				details="La qualité du service est au cœur de notre engagement chez Kasa. Nous
 					veillons à ce que chaque interaction, que ce soit avec nos hôtes ou
 					nos locataires, soit empreinte de respect et de bienveillance."
+				classSummary="collapse__summary"
+				classDetails="collapse__details"
 			/>
 			<Collapse
 				summary="Sécurité"
@@ -27,6 +33,8 @@ export default function AboutValues() {
 					l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les
 					standards sont bien respectés. Nous organisons également des ateliers
 					sur la sécurité domestique pour nos hôtes."
+				classSummary="collapse__summary"
+				classDetails="collapse__details"
 			/>
 		</div>
 	);
