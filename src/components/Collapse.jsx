ea938f036d381a0c446a550eff22371f05cacd0d
noscatis <michael.bocquel@gmail.com>
2024-12-02 15:46:32 +0100

    fix: add collapse datails text in p tag

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,8 +1,12 @@
 export default function Collapse(props) {
 	return (
-		<details className={props.classDetails}>
-			<summary className={props.classSummary}>{props.summary}</summary>
-			<div>{props.details}</div>
-		</details>
+		<div className="collapse">
+			<details className={props.classDetails}>
+				<summary className={props.classSummary}>{props.summary}</summary>
+			</details>
+			<div className="collapse__details__div">
+				<p className="collapse__details__text">{props.details}</p>
+			</div>
+		</div>
 	);
 }
